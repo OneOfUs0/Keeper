862fa8f637f3aaa7dfc23d44835f11cf0ce76400
OneOfUs0 <greg.nichols@icf.com>
2024-05-24 21:07:58 -0700

    Initial commit and push

--- main.py
+++ main.py
@@ -1,48 +1,119 @@
-# This is a sample Python script.
+
 import datetime
 import streamlit as st
-# import pandas as pd
+import random
+import pandas as pd
+import string
 
 # -------------------- development notes --------------------
 #  To run this from Pycharm, do this:
 #   1. open in PyCharm - (already here)
-#  2.  go to the Terminal and type "streamlit run main.py"
-#  To refresh the app, use F5.
-#
+#  2.  go to the Terminal and cd to this folder.
+#  3. type "streamlit run main.py"
+#  4. on the app, set setting to "Run on Save"
 #
 # ---------------------------------------------------------
 
-def start_onclick():
-    print('starting timer')
-    # record the start time.
-    _starttime = datetime.datetime.now()
+# ================================== INIT  =========================================
 
-    now = datetime.datetime.now()
+# initialize
+if 'status' not in st.session_state:
+    st.session_state.status = 'resting'
+    print('----> init status')
+if 'starttime' not in st.session_state:
+    st.session_state.starttime = ''
+if 'stoptime' not in st.session_state:
+    st.session_state.stoptime = ''
 
+# ================================== FUNCTIONS =========================================
+@st.cache_data
+def GetProjects():
+
+    records = []
+    for i in range(0,10):
+        code = ''
+        for cc in range(0,4):
+            for c in range(0,3):
+                code += str(random.randint(2,99))
+            code += '.'
+
+        name = ''
+        for x in range(0,25):
+            name += list(string.ascii_lowercase)[random.randint(0,23)]
+        rec = {'code':code.strip('.'),'name':name}
+
+        records.append(rec)
+
+    df = pd.DataFrame(records)
+    return df
+
+# ================================== UI CALLBACKS =========================================
 
 def stop_onclick():
-    print('stopping timer')
+    # STOP          STOP         STOP
+
+    print('STOP RUN')
+
+    if st.session_state.status == 'working':
+        st.session_state.status = 'resting'
+        st.session_state.stoptime = datetime.datetime.now()
+        print('Stopped timer')
+
+
     # calculate elapsed time
 
     # record the time elapsed on that project.
 
+def start_onclick():
+    # START         START         START
 
+    print('START RUN')
+
+    # START
+    if st.session_state.status == 'resting':
+        st.session_state.status = 'working'
+        st.session_state.starttime = datetime.datetime.now()
+        print('Started timer')
+
+
+# ===============================  UI  ===========================================
 # LAYOUT
-st.title('Project Time Keepers')
+st.title('Project Time Keeper')
+
 st.divider()
-contain = st.container()
-contain.button('inside')
-contain.button('inside right', disabled=True)
+cont_startstop = st.container()
 
-btn_start = st.button('Start',on_click=start_onclick())
-btn_stop = st.button('Stop',on_click=stop_onclick(),disabled=True)
+cont_startstop.subheader(st.session_state.status)
+
+if st.session_state.status == 'rest':
+    notworking = True
+    notresting = False
+
+if st.session_state.status == 'work':
+    notworking = False
+    notresting = True
 
 
+startwork = cont_startstop.button('Start',key='btn_start',on_click=start_onclick(),disabled=False)
+stopwork = cont_startstop.button('Stop',key='btn_stop',on_click=stop_onclick(),disabled=False)
+
+
+st.divider()
+
+st.dataframe(GetProjects(),use_container_width=True)
+
+cont_newProject = st.container()
+cont_newProject.subheader("Add New Project Information")
+cont_newProject.text_input('Billing Code',key='ti_newcode')
+cont_newProject.text_input('Name (what you want to call it',key='ti_newname')
+
+# ===============================  MAIN  ===========================================
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    global _starttime
+    print('---- MAIN ----')
+    print('status:' + st.session_state['status'])
 
 
 
